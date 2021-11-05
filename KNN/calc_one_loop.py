cd3fd4835a0a52f81d51b7acded307df2beb3ba7
Aminzein <Aminzein@users.Zforce.github.com>
2021-11-05 21:33:56 +0330

    finished functions now starting to put them together in a class

--- KNN/calc_one_loop.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import numpy as np
-def calc_one_loop(new_points, points):
-    m, n = len(new_points), len(points)    
-    d = np.zeros((m, n))
-    for i in range(m):
-        d[i] = np.sum((new_points[i] - points) ** 2 , axis=1)
-    return d
--- KNN/calc_two_loops.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import numpy as np
-def calc_two_loops(new_points, points):
-    m, n = len(new_points), len(points)
-    d = np.zeros((m, n))
-    for i in range(m):
-        for j in range(n):
-            d[i, j] = np.sum((new_points[i] - points[j]) ** 2)
-    return d
